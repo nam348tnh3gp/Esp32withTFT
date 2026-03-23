e63c40935e00d23e734c6c239f7c53c54410ca62
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-03-24 04:45:36 +0700

    Create User_Setup_Select.h

---
+++ User_Setup_Select.h
@@ -0,0 +1,16 @@
+// Tìm file: Documents/Arduino/libraries/TFT_eSPI/User_Setup.h
+// Hoặc tạo file User_Setup_Select.h với nội dung:
+
+#define ST7789_DRIVER
+
+#define TFT_CS   8
+#define TFT_DC   9
+#define TFT_RST  10
+#define TFT_SCLK 12
+#define TFT_MOSI 11
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
+
+#define SPI_FREQUENCY  40000000
+#define SPI_READ_FREQUENCY  20000000
